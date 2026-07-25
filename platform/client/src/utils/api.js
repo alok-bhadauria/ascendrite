cc1cef78fa62666f5a091a949da3d2b3ac287a12
Alok Bhadauria <bhadauriaalok123@gmail.com>
2026-07-26 04:21:26 +0530

    fix(client): unwrap envelope route response payloads automatically in Axios client

--- platform/client/src/utils/api.js
+++ platform/client/src/utils/api.js
@@ -5,4 +5,18 @@ const api = axios.create({
   withCredentials: true
 });
 
+// Response interceptor to unwrap EnvelopeRoute payloads
+api.interceptors.response.use(
+  (response) => {
+    // If the response is wrapped by the EnvelopeRoute middleware, unwrap the data
+    if (response.data && typeof response.data === 'object' && response.data.success === true && 'data' in response.data) {
+      response.data = response.data.data;
+    }
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
